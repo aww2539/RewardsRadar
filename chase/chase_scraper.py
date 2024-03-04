4a5d1b2b74f9f4b3c5aa0cd59a97d3191b5e587a
Alex Woodall <aww2539@gmail.com>
2024-03-04 14:34:48 -0500

    reorganized dirs, files, and imports

--- chase/chase_scraper.py
+++ chase/chase_scraper.py
@@ -1,17 +1,26 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-from chase_cards import card_rewards_html_ids
-def remove_html(string):
-    return re.sub(r'<[^>]*>', "", string, 0).strip()
+
+card_data = [
+        {"name": "Chase Sapphire Preferred", "id": "t-r6-4"},
+        {"name": "Chase Sapphire Reserve", "id": "t-r6-37"},
+        {"name": "Chase Freedom Unlimited", "id": "t-r6-36"},
+        {"name": "Chase Freedom Flex", "id": "t-r6-56"},
+        {"name": "Chase Freedom Rise", "id": "t-r6-71"}
+    ]
 
 chase_url = "https://creditcards.chase.com/compare-credit-cards"
 chase_page = requests.get(chase_url)
 soup = BeautifulSoup(chase_page.content, "html5lib")
 
+
+def remove_html(string):
+    return re.sub(r'<[^>]*>', "", string, 0).strip()
+
 def scrape_chase_cards():
-    for card in card_rewards_html_ids:
-        with open('chase_rewards.txt', 'a') as f:
+    for card in card_data:
+        with open('chase/output/chase_rewards.txt', 'a') as f:
             f.write("{name}".format(name = card["name"]))
             f.write("\n")
 
@@ -24,7 +33,6 @@ def scrape_chase_cards():
                     sup_tag_regex = r'<su[^>].+?</sup>'
                     sup_tags = re.findall(sup_tag_regex, string)
 
-
                     if sup_tags:
                         result = re.sub(sup_tag_regex, "", string, 0)
                         s = remove_html(result)
@@ -34,6 +42,10 @@ def scrape_chase_cards():
                     f.write("{s}".format(s=s))
                     f.write("\n")
             f.write("\n")
-        f.close()
+    f.close()
 
-scrape_chase_cards()
+
+# Specific compare URLs if needed for later use
+# freedom_cards_url = "https://creditcards.chase.com/compare-credit-cards/?iCELL=61FZ&list=36%2C56%2C71"
+# sapphire_cards_url = "https://creditcards.chase.com/compare-credit-cards/?iCELL=61FZ&list=36%2C56%2C71"
+# amazon_cards_url = "https://creditcards.chase.com/compare-credit-cards?list=22%2C66&iCELL=6C1Y"
---
+++ scrape.py
@@ -0,0 +1,6 @@
+from chase.chase_scraper import scrape_chase_cards
+
+def execute():
+    scrape_chase_cards()
+
+execute()
